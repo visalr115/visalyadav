61a3511dc874f030e61810a05593363ed8f25f45
visalyadav115 <visal.yadav@redlooptech.com>
2017-11-09 18:13:09 +0530

    codes for header

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <h2>Visal Yadav</h2>
+          <h2 className="myname" >Visal Yadav</h2>
         </header>
       </div>
     );
