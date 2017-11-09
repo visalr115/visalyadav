12305820e1a68fb04d9e22246540959141342219
visalyadav115 <visal.yadav@redlooptech.com>
2017-11-09 20:20:23 +0530

    header completed

--- src/App.js
+++ src/App.js
@@ -6,8 +6,22 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
+          <i className="fa fa-bars"/>
           <h2 className="myname" >Visal Yadav</h2>
+          <a href="https://github.com/visalr115"><i className="fa fa-github" /></a>
+          <a href="https://www.linkedin.com/in/visalyadav/"><i className="fa fa-linkedin" /></a> 
         </header>
+        <div className="navbar">
+          <ul className="list">
+            <li>Home</li>
+            <li>About</li>
+            <li>Skills</li>
+            <li>Portfolios</li>
+            <li>Contact</li>
+          </ul>
+        </div>
+        <div className="container">
+          </div>
       </div>
     );
   }
