c96e44ed69493093622ed124e31d9b266d6effd7
visalyadav115 <visal.yadav@redlooptech.com>
2017-11-15 13:13:40 +0530

    completed portfolio

--- src/containers/Portfolios.js
+++ src/containers/Portfolios.js
@@ -16,6 +16,30 @@ const Portfolios = () => (
           </ul>
         </div>
         <div className="container"/>
+        <div className="port1">
+      <div className="port1head" />
+      <h2 className="title">Leopetra website</h2>
+      <p className="subdeco">
+        Worked on Leopetra website using react.js.<br />
+        Full​ ​Responsive​ ​Website​,​ ​with​ ​admins​ ​having​ ​ability​ ​to  update​ ​content​ ​on​ ​server.<br />
+        A fully functional website where one can avail any information about pets and also can book
+        Vets, Trainers, Saloon, Restaurants and Event tickets for their pets.
+      </p>
+      <hr />
+      <a className="weblink" href="https://leopetra.com/">VISIT LEOPETRA WEBSITE</a>
+    </div>
+    <div className="port2">
+      <div className="port2head" />
+      <h2 className="title">WeberHead Design</h2>
+      <p className="subdeco">
+        This is my First website.<br />
+        Worked on this website using html5 and css3.<br />
+        Full​ ​Responsive​ Designed ​Website​.<br />
+        Only for try my skills.
+      </p>
+      <hr />
+      <a className="weblink" href="https://weberhead.herokuapp.com/">VISIT THE WEBSITE</a>
+    </div>
       </div>
 );
 export default Portfolios;
