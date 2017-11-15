547b80b723870197332689fd586ef00c42f30268
visalyadav115 <visal.yadav@redlooptech.com>
2017-11-15 17:43:13 +0530

    my portfolio

--- src/containers/About.js
+++ src/containers/About.js
@@ -23,21 +23,21 @@ const About = () => (
           <Fade left>
           <h2>Hi !</h2>
           <p>
-        My name is Visal Yadav and I've completed my B-Tech degree in Electronics & Communication Engineering
-        from Calcutta Institute of Technology, Howrah.I joined CIT in 2011. Joining CIT was great for me in my life. I completed my degree in 2015.
-      </p>
-      <p>
-      I am currently searching for an opportunity to fulfill my goal and to use my knowledge for
-      the development of the firm. I build interactive website that run across Platform and Device
-      </p> 
-      <p>
-      I like exploring new tech articles on the internet, to keep myself updated with the recent
-      technology. My interest lies in knowing more about Cloud Computing, Web Technology.
-      </p>
-      <p>
-      In my free time, I like to share my knowledge on Quora. Yes! I like games I play FIFA on my playstation.
-      I also play Cricket.
-      </p>
+            My name is Visal Yadav and I've completed my B-Tech degree in Electronics & Communication Engineering
+            from Calcutta Institute of Technology, Howrah.I joined CIT in 2011. Joining CIT was great for me in my life. I completed my degree in 2015.
+         </p>
+          <p>
+            I am currently searching for an opportunity to fulfill my goal and to use my knowledge for
+            the development of the firm. I build interactive website that run across Platform and Device
+          </p> 
+          <p>
+            I like exploring new tech articles on the internet, to keep myself updated with the recent
+            technology. My interest lies in knowing more about Cloud Computing, Web Technology.
+          </p>
+          <p>
+            In my free time, I like to share my knowledge on Quora. Yes! I like games I play FIFA on my playstation.
+            I also play Cricket.
+          </p>
       <hr />
       <a href="https://drive.google.com/file/d/0BwaHyFnWjuFRY0pTUjFzRmt6TTg/view?usp=sharing"><h3>CLICK HERE TO VIEW MY RESUME</h3></a>
       </Fade>
